30d6fbe0939b9040067b8c477b92c507c0547b72
Hans Poo <hchpoo@falabella.cl>
2023-06-01 09:31:49 -0400

    Mejora Readme

--- apps/app1/src/app/app.tsx
+++ apps/app1/src/app/app.tsx
@@ -4,11 +4,7 @@ import styles from './app.module.css';
 import NxWelcome from './nx-welcome';
 
 export function App() {
-  return (
-    <div>
-      <NxWelcome title="app1" />
-    </div>
-  );
+  return <div>Var vale: {import.meta.env.VITE_REACT1}</div>;
 }
 
 export default App;
